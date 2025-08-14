2ffcc634b94a6c508d58fb8ea2a257c1eb685bc8
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2025-08-14 23:27:15 +0530

    add Cloudinary Config

---
+++ server/config/cloudinary.js
@@ -0,0 +1,21 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'travel_destinations',
+    resource_type: 'auto' // supports both images and videos
+  }
+});
+
+const upload = multer({ storage });
+
+module.exports = { cloudinary, upload };
